e2df5e36c3cc8f41383ec2acd70bf0a1dec103ce
Nottrex <Lau.Nottrex@gmail.com>
2017-03-25 22:57:26 +0100

    Test

--- src/game/Game.java
+++ src/game/Game.java
@@ -20,7 +20,7 @@ public class Game {
 	private int playerTurn;
 
 	public Game() {
-		map = new GameMap(new MapGenerator(new HexPreset(51, 51)));
+		map = new GameMap(new MapGenerator(new HexPreset(61, 61)));
 		playerAmount = 2;
 		players = new PlayerColor[]{PlayerColor.BLUE, PlayerColor.RED};
 	}
