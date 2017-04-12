b3da91fc5ed75c30ee3f53f27a09e38c031390fc
PhoenixofForce <phoenixofforce@gmail.com>
2017-04-12 18:22:09 +0200

    [+] Options are saved on Close and loaded on Opening

--- src/client/Main.java
+++ src/client/Main.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+        Options.load();
         SwingUtilities.invokeLater(() -> new Window());
     }
-
 }
---
+++ src/client/Options.java
@@ -0,0 +1,63 @@
+package client;
+
+import client.audio.AudioConstants;
+import client.window.GUIConstants;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+
+import java.awt.*;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Options {
+
+    private static final File DATA_FILE = new File("options.yml");
+
+    public static void save() {
+        DumperOptions op = new DumperOptions();
+        op.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        Yaml yaml = new Yaml(op);
+
+        Map<String, Object> data = new HashMap<>();
+
+        Map<String, Float> audioData = new HashMap<>();
+        audioData.put("Music", AudioConstants.MUSIC_VOLUME);
+        audioData.put("Effects", AudioConstants.EFFECT_VOLUME);
+
+        Map<String, Boolean> guiData = new HashMap<>();
+        guiData.put("AA", GUIConstants.VALUE_ANTIALIASING.equals(RenderingHints.VALUE_ANTIALIAS_ON));
+
+        data.put("Audio", audioData);
+        data.put("Graphics", guiData);
+
+        try {
+            yaml.dump(data, new FileWriter(DATA_FILE));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void load() {
+        DumperOptions op = new DumperOptions();
+        op.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        Yaml yaml = new Yaml(op);
+        if(DATA_FILE.exists()) {
+
+            try {
+                Map<String, Object> data = (Map<String, Object>) yaml.load(new FileInputStream(DATA_FILE));
+
+                Map<String, Double> audioData = (Map<String, Double>) data.get("Audio");
+                AudioConstants.MUSIC_VOLUME = audioData.get("Music").floatValue();
+                AudioConstants.EFFECT_VOLUME = audioData.get("Effects").floatValue();
+
+                Map<String, Boolean> guiData = (Map<String, Boolean>)data.get("Graphics");
+                GUIConstants.VALUE_ANTIALIASING = guiData.get("AA")? RenderingHints.VALUE_ANTIALIAS_ON: RenderingHints.VALUE_ANTIALIAS_OFF;
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
