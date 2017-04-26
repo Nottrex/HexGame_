552c90a3ae306030aa3026fc485367d1c67eb94d
PhoenixofForce <phoenixofforce@gmail.com>
2017-04-26 20:28:29 +0200

    [/] Changed SquareMaps to real squares

--- src/game/map/presets/OvalPreset.java
+++ src/game/map/presets/OvalPreset.java
@@ -16,15 +16,15 @@ public class OvalPreset implements MapPreset{
 
 	@Override
 	public Field[][] getPresetMap() {
-		Field[][] out = new Field[width][height];
+		Field[][] out = new Field[2*width][height];
 
-		float a = width / 2;
+		float a = width;
 		float b = height/2;
 		for(int x = 0; x < out.length; x++) {
 			for (int y = 0; y < out[0].length; y++) {
 				float x2 = x - width/2;
 				float y2 = y - height/2;
-				if(!(((x2*x2)/(a*a) + (y2*y2)/(b*b)) <= 1.0f)) out[x][y] = Field.VOID;
+				if(!(((x2*x2)/(a*a) + (y2*y2)/(b*b)) <= 1.0f)) out[2*x][y] = Field.VOID;
 			}
 		}
 
--- src/game/map/presets/SquarePreset.java
+++ src/game/map/presets/SquarePreset.java
@@ -21,7 +21,21 @@ public class SquarePreset implements  MapPreset{
 
     @Override
     public Field[][] getPresetMap() {
-        return new Field[width][height];
+        Field[][] out = new Field[width*2][height];
+
+        for(int  x = 0; x < width*2; x++) {
+            for(int  y = 0; y < height; y++) {
+                out[x][y] = Field.VOID;
+            }
+        }
+
+        for(int  x = 0; x < width; x++) {
+            for(int  y = 0; y < height; y++) {
+                out[x+y/2][y] = null;
+            }
+        }
+
+        return out;
     }
 
     @Override
@@ -29,9 +43,9 @@ public class SquarePreset implements  MapPreset{
         List<Location> locations = new ArrayList<>();
 
         locations.add(new Location(2, 2));
-        locations.add(new Location(width-3, height-3));
+        locations.add(new Location(width-3, height-3)); //TODO: Fix
         locations.add(new Location(2, height-3));
-        locations.add(new Location(width-3, 2));
+        locations.add(new Location(width-3, 2));        //TODO: Fix
 
         return locations;
     }
