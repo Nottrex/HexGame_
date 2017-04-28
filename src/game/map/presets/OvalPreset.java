b3f681ab0cfc1bafa7f4ad95db5cc17caaf64ae7
PhoenixofForce <phoenixofforce@gmail.com>
2017-04-28 16:21:29 +0200

    [/] Real ovals

--- src/game/map/presets/OvalPreset.java
+++ src/game/map/presets/OvalPreset.java
@@ -16,15 +16,23 @@ public class OvalPreset implements MapPreset{
 
 	@Override
 	public Field[][] getPresetMap() {
-		Field[][] out = new Field[2*width][height];
+		Field[][] out = new Field[width + height][height];
 
-		float a = width;
-		float b = height/2;
 		for(int x = 0; x < out.length; x++) {
-			for (int y = 0; y < out[0].length; y++) {
+			for(int y = 0; y < out[0].length; y++) {
+				out[x][y] = Field.VOID;
+			}
+		}
+
+		float a = width/2;
+		float b = height/2;
+		for(int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
 				float x2 = x - width/2;
 				float y2 = y - height/2;
-				if(!(((x2*x2)/(a*a) + (y2*y2)/(b*b)) <= 1.0f)) out[2*x][y] = Field.VOID;
+				if((((x2*x2)/(a*a) + (y2*y2)/(b*b)) <= 1.0f)) {
+					out[x + y/2][y] = null;
+				}
 			}
 		}
 
@@ -34,7 +42,8 @@ public class OvalPreset implements MapPreset{
 	@Override
 	public List<Location> getSpawnPoints() {
 		List<Location> locations = new ArrayList<>();
-
+		locations.add(new Location(2, 2));
+		locations.add(new Location(3, 3));
 		//TODO: getSpawnPoints
 		return locations;
 	}
